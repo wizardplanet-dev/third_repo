4702cad10656a9747ce3d36329ed7595dc0b2eb5
wizardplanet-dev <wizardplanet123@gmail.com>
2026-04-12 00:10:54 +0300

    latest

--- src/java/java-file.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello dev planet");
-    final int NUM = 12;
-    final int MULTIPLIER = 100;
-    int num2 = MULTIPLIER * NUM;
-    System.out.println("Number is: " + num2);
-  }
-}
