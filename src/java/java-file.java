81623e61a30004e41959138a676435dda0d0b45f
wizardplanet-dev <wizardplanet123@gmail.com>
2026-04-11 18:56:46 +0300

    Added new java file

---
+++ src/java/java-file.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello dev planet");
+    final int NUM = 12;
+    final int MULTIPLIER = 100;
+    int num2 = MULTIPLIER * NUM;
+    System.out.println("Number is: " + num2);
+  }
+}
